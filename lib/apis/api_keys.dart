e138dacd4fa59599624157a1b4f4364eb781143c
Albert Borràs <albertborraspons@gmail.com>
2023-07-12 18:51:37 +0200

    api keys update 2

--- lib/apis/api_keys.dart
+++ lib/apis/api_keys.dart
@@ -1,3 +1,3 @@
+const String OPENAI_API_KEY = "put_your_openai_api_key_here";
 const List<String> UNSPLASH_API_KEYS = ["4WQXIIc6UR6xkjquhXQcDpa5Bxs8ZRzhTrD7V38nxPo", "3p6PwFsrup5Xvd2FndtBKzUWveAFSHFbtsQ7rAK-6ks", "0AtAXT1pbqUCFW6BTfpSrskczX8O1hUbUJjlrAJ6DFQ", "7OgS1y1l06yqwdWmcl58vOjzTaNgj1SG3F_qccWuJL8", "ooQXPgU7E7Ayjss3sCzaDAUfT22rklt1YkxcE0UNxsw"];
-const String OPENAI_API_KEY = "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";
 const String GOOGLE_CLOUD_TRANSLATION_API_KEY = "AIzaSyBF9rpISYhI4KLA0svuGBgoItcbWkQiLy4";
