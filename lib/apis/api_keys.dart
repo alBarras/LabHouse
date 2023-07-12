0043026ff67636dab7fe2efd96fd782f3f7d5940
Albert Borràs <albertborraspons@gmail.com>
2023-07-12 18:35:47 +0200

    minor changes

--- lib/apis/api_keys.dart
+++ lib/apis/api_keys.dart
@@ -1,4 +1,4 @@
 //this file normally would NOT be uploaded to github (would be in .gitignore)
 const List<String> UNSPLASH_API_KEYS = ["4WQXIIc6UR6xkjquhXQcDpa5Bxs8ZRzhTrD7V38nxPo", "3p6PwFsrup5Xvd2FndtBKzUWveAFSHFbtsQ7rAK-6ks", "0AtAXT1pbqUCFW6BTfpSrskczX8O1hUbUJjlrAJ6DFQ", "7OgS1y1l06yqwdWmcl58vOjzTaNgj1SG3F_qccWuJL8", "ooQXPgU7E7Ayjss3sCzaDAUfT22rklt1YkxcE0UNxsw"];
-const String OPENAI_API_KEY = "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";
+const String OPENAI_API_KEY = "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";
 const String GOOGLE_CLOUD_TRANSLATION_API_KEY = "AIzaSyBF9rpISYhI4KLA0svuGBgoItcbWkQiLy4";
